b9ea594dc7c36638d8c90cb420dea2c6fc63eeab
unkprog <unkprog@gmail.com>
2021-07-20 02:39:29 +0300

    Форма выбора ингридиентов

--- PizzaDaMamma.Orders/Pages/Index.cs
+++ PizzaDaMamma.Orders/Pages/Index.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.Json;
 using Microsoft.AspNetCore.Hosting;
@@ -17,12 +19,46 @@ namespace PizzaDaMamma.Orders.Pages
             if (File.Exists(file))
             {
                 result = readFileAsString(file);
+                string dataItemsStr = loadIndexJson(env);
+                MenuItemModel[] dataItems = ToItems(dataItemsStr);
+                result = result.Replace("<!--here-->", build(dataItems));
 
-                MenuItemModel[] items = loadData(env);
+                Dictionary<string, string> ingridientsAll = new Dictionary<string, string>();
+                string ingr;
+                foreach(var item in dataItems)
+                {
+                    foreach(var itemIngr in item.Ingridients)
+                    {
+                        if (!ingridientsAll.TryGetValue(itemIngr, out ingr))
+                            ingridientsAll.Add(itemIngr, itemIngr);
+                    }
+                }
 
-                result = result.Replace("<!--here-->", build(items));
+                result = result.Replace("//<!--data here-->", "window.Index.Data.Items = " + dataItemsStr + "; window.Index.Data.Ingridients = " + JsonSerializer.Serialize(ingridientsAll.Values.ToArray()) + ";");
+                
             }
             return result;
+        }
+
+        private static string loadIndexJson(IWebHostEnvironment env)
+        {
+            string result = string.Empty;
+
+            System.Type t = typeof(Startup);
+            string file = string.Concat(t.Assembly.Location.Replace(t.Assembly.ManifestModule.Name, string.Empty), env.IsDevelopment() ? @"..\..\..\" : string.Empty, @"wwwroot\data\index.json");
+            if (File.Exists(file))
+                result = readFileAsString(file);
+            return result;
+        }
+
+        private static MenuItemModel[] ToItems(string json)
+        {
+            var options = new JsonSerializerOptions
+            {
+                AllowTrailingCommas = true,
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(System.Text.Unicode.UnicodeRanges.All) // System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+            };
+            return JsonSerializer.Deserialize<MenuItemModel[]>(json, options);
         }
 
         private static string build(MenuItemModel[] items)
@@ -35,11 +71,11 @@ namespace PizzaDaMamma.Orders.Pages
                 string desc = string.Join(", ", item.Ingridients);
                 string html = @$"
         <div class='pdm-col'>
-            <div class='card-header'>
+            <h1 class='card-header'>
                 {item.Name}
-            </div>
+            </h1>
             <div class='card-image'>
-                <img src='img/menu/{item.Image}' />
+                <img src='/img/menu/{item.Image}' />
             </div>
             <div class='card-description'>
                 {desc}
@@ -79,7 +115,7 @@ namespace PizzaDaMamma.Orders.Pages
                 <tbody>
                     <tr class='card-table-footer-row'>
                         <td class='card-table-footer-td card-table-footer-price'>{item.Price}</td>
-                        <td class='card-table-footer-td card-table-footer-button'>Выбрать</td>
+                        <td class='card-table-footer-td card-table-footer-button' data-index='{i}'>Выбрать</td>
                     </tr>
                 </tbody>
             </table>
@@ -90,25 +126,7 @@ namespace PizzaDaMamma.Orders.Pages
             return result.ToString();
         }
 
-        private static MenuItemModel[] loadData(IWebHostEnvironment env)
-        {
-            MenuItemModel[] result = null;
-
-            System.Type t = typeof(Startup);
-            string file = string.Concat(t.Assembly.Location.Replace(t.Assembly.ManifestModule.Name, string.Empty), env.IsDevelopment() ? @"..\..\..\" : string.Empty, @"wwwroot\data\index.json");
-            if (File.Exists(file))
-            {
-                string json = readFileAsString(file);
-                var options = new JsonSerializerOptions
-                {
-                    AllowTrailingCommas = true,
-                    Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(System.Text.Unicode.UnicodeRanges.All) // System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
-                };
-
-                result = JsonSerializer.Deserialize<MenuItemModel[]>(json, options);
-            }
-            return result;
-        }
+        
 
         private static string readFileAsString(string aFileName, Encoding enc = null)
         {
---
+++ PizzaDaMamma.Orders/wwwroot/js/Index.js
@@ -0,0 +1,122 @@
+﻿window.Index = {
+    Post: function (url, data, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                if (callback)
+                    callback(xhr.responseText);
+            }
+        };
+        xhr.send(data);
+    },
+    Data: {
+        Items: [],
+        Ingridients: []
+    },
+    Controls: {
+        pdmModal: undefined,
+    },
+
+    Init: function () {
+        let self = window.Index;
+        let buttons = document.body.querySelectorAll(".card-table-footer-button");
+
+        self.Controls.pdmModal = document.querySelectorAll('.pdm-modal')[0];
+
+        buttons.forEach(function (button) {
+            button.addEventListener("click", function (e) {
+                var dataIndex = parseInt(e.target.getAttribute('data-index'));
+                self.ShowSelectItem(dataIndex);
+            });
+        });
+
+        let buttonsClose = document.body.querySelectorAll(".pdm-modal-close");
+        buttonsClose.forEach(function (buttonClose) {
+            buttonClose.addEventListener("click", function (e) {
+                self.Controls.pdmModal.style.display = 'none';
+            });
+        });
+    },
+
+    ShowSelectItem: function (index) {
+        let self = window.Index;
+        if (index < 0 || index >= self.Data.Items.length)
+            return;
+
+        let item = self.Data.Items[index];
+        let pdmSelectHeader = document.querySelectorAll('#pdm-pizza-select-header')[0];
+        pdmSelectHeader.innerHTML = item.Наименование;
+
+        let pdmSelectImage = document.querySelectorAll('#pdm-pizza-select-image')[0];
+        pdmSelectImage.src = "/img/menu/" + item.Изображение;
+
+        let html = '';
+        for (let i = 0, icount = item.Ингридиенты.length; i < icount; i++) {
+            html += "<span class='pdm-pizza-select-ingridient pdm-pizza-select-ingridient-selected'>" + item.Ингридиенты[i] + "</span>";
+        }
+
+        let pdmSelectIngridients = document.querySelectorAll("#pdm-pizza-select-ingridients")[0];
+        pdmSelectIngridients.innerHTML = html;
+
+
+        html = '';
+        for (let i = 0, icount = self.Data.Ingridients.length; i < icount; i++) {
+            if (item.Ингридиенты.indexOf(self.Data.Ingridients[i]) < 0)
+                html += "<span class='pdm-pizza-select-ingridient'>" + self.Data.Ingridients[i] + "</span>";
+        }
+
+        let pdmSelectIngridientsAdd = document.querySelectorAll("#pdm-pizza-select-ingridients-add")[0];
+        pdmSelectIngridientsAdd.innerHTML = html;
+
+
+
+        let buttonsSelect = document.body.querySelectorAll(".pdm-pizza-select-ingridient");
+        buttonsSelect.forEach(function (buttonClose) {
+            buttonClose.addEventListener("click", self.SelectIngridient);
+        });
+
+        self.Controls.pdmModal.style.display = 'block';
+    },
+
+    SelectIngridient: function (e) {
+        var ingr = e.target.innerHTML;
+        if (e.target.hasClass("pdm-pizza-select-ingridient-selected"))
+            e.target.removeClass("pdm-pizza-select-ingridient-selected");
+        else
+            e.target.addClass("pdm-pizza-select-ingridient-selected");
+    },
+};
+
+HTMLElement.prototype.hasClass = function (cls) {
+    var i;
+    var classes = this.className.split(" ");
+    for (i = 0; i < classes.length; i++) {
+        if (classes[i] == cls) {
+            return true;
+        }
+    }
+    return false;
+};
+
+HTMLElement.prototype.addClass = function (add) {
+    if (!this.hasClass(add)) {
+        this.className = (this.className + " " + add).trim();
+    }
+};
+
+HTMLElement.prototype.removeClass = function (remove) {
+    var newClassName = "";
+    var i;
+    var classes = this.className.replace(/\s{2,}/g, ' ').split(" ");
+    for (i = 0; i < classes.length; i++) {
+        if (classes[i] !== remove) {
+            newClassName += classes[i] + " ";
+        }
+    }
+    this.className = newClassName.trim();
+};
+document.body.onload = function (e) {
+    window.Index.Init();
+};
